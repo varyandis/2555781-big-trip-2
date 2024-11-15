cb2295379889ee1fbe7bddc7383b7c8766e46ab6
Alexandra Glen <varyandis@yandex.ru>
2024-11-15 12:40:18 +0300

    7.9 Добавит презентер фильтров

--- src/main.js
+++ src/main.js
@@ -1,32 +1,30 @@
 import {render, RenderPosition} from '../src/framework/render.js';
 // import {render, RenderPosition} from './render.js';
 import TripMainInfoView from './view/info-view.js';
-import TripFiltersView from './view/filters-view.js';
+// import FilterPresenter from './presenter/filter.js';
+import FilterPresenter from './presenter/filter.js';
 import Board from './presenter/board.js';
 import PointsModel from './model/points-model.js';
 import FilterModel from './model/filter-model.js';
-// import { generateFilter } from './mock/filter.js';
-
-const filters = [
-  {
-    type: 'everything',
-    count: 0,
-  },
-];
 
 const siteMainSortElement = document.querySelector('.trip-events');
 const siteMainFiltersElement = document.querySelector('.trip-controls__filters');
 const siteMainInfoElement = document.querySelector('.trip-main');
 const pointsModel = new PointsModel();
 const filterModel = new FilterModel();
-
+const BoardPresenter = new Board({boardContainer: siteMainSortElement, pointsModel});
+const filterPresenter = new FilterPresenter({
+  filterContainer: siteMainFiltersElement,
+  filterModel,
+  pointsModel
+});
 // const filters = generateFilter(pointsModel.point);
 
 render(new TripMainInfoView(), siteMainInfoElement , RenderPosition.AFTERBEGIN);
-render(new TripFiltersView({filters,
-  currentFilterType: 'aleverythingl',
-  onFilterTypeChange: () => {}}), siteMainFiltersElement , RenderPosition.AFTERBEGIN);
+// render(new TripFiltersView({filters,
+//   currentFilterType: 'aleverythingl',
+//   onFilterTypeChange: () => {}}), siteMainFiltersElement , RenderPosition.AFTERBEGIN);
 
-const BoardPresenter = new Board({boardContainer: siteMainSortElement, pointsModel});
 
+filterPresenter.init();
 BoardPresenter.init();
